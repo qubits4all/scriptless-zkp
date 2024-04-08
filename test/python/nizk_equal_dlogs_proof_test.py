07be1cf55fc39b875d19eec9d3222b7f203767f4
Will D. Spann <qubits4all@users.noreply.github.com>
2024-04-08 01:26:15 -0600

    [zkp] Tests for invalid ZK proofs
    - Added test cases for several invalid ZK proofs re: the
    `nizk_equal_dlogs_proof` module, corresponding to recent improvements to
    proof verification logic.
    
    GI-11

--- test/python/nizk_equal_dlogs_proof_test.py
+++ test/python/nizk_equal_dlogs_proof_test.py
@@ -21,7 +21,8 @@ from Cryptodome.PublicKey import ECC
 from scriptless_zkp.ecc.weierstrass_curves import WeierstrassEllipticCurveConfig
 from scriptless_zkp.ecc.zkp.nizk_equal_dlogs_proof import (
     NIZKEqualDiscreteLogsContext, NIZKEqualDiscreteLogsProver, NIZKDiscreteLogParameterSet,
-    ECCDiscreteLogBaseAndProduct, NIZKEqualDiscreteLogsProof
+    ECCDiscreteLogBaseAndProduct, NIZKEqualDiscreteLogsProof, NIZKEqualDiscreteLogsProofHash,
+    NIZKEqualDiscreteLogsProofSignature
 )
 
 
@@ -74,6 +75,62 @@ class NIZKEqualDiscreteLogsProofTests(unittest.TestCase):
         self.assertIsInstance(zk_proof, NIZKEqualDiscreteLogsProof)
 
         self.assertTrue(zk_proof.verify())
+
+    def test_invalid_proof_zero_public_hash(self):
+        invalid_proof_pub_hash = NIZKEqualDiscreteLogsProofHash(0)
+        constant_proof_sig = NIZKEqualDiscreteLogsProofSignature(1)
+
+        invalid_proof = NIZKEqualDiscreteLogsProof(
+            self.dlog_params_set,
+            invalid_proof_pub_hash,
+            constant_proof_sig,
+            hash_algorithm=NIZKEqualDiscreteLogsContext.DEFAULT_HASH_ALGORITHM
+        )
+        print(f"NIZK Equal Discrete Logs Proof (Invalid Public Hash): {invalid_proof}\n")
+        self.assertFalse(invalid_proof.verify())
+
+    def test_invalid_proof_zero_signature(self):
+        invalid_proof_sig = NIZKEqualDiscreteLogsProofSignature(0)
+        constant_proof_pub_hash = NIZKEqualDiscreteLogsProofHash(1)
+
+        invalid_proof = NIZKEqualDiscreteLogsProof(
+            self.dlog_params_set,
+            constant_proof_pub_hash,
+            invalid_proof_sig,
+            hash_algorithm=NIZKEqualDiscreteLogsContext.DEFAULT_HASH_ALGORITHM
+        )
+        print(f"NIZK Equal Discrete Logs Proof (Invalid Proof Signature): {invalid_proof}\n")
+        self.assertFalse(invalid_proof.verify())
+
+    def test_invalid_proof_nonce_inf_point(self):
+        # Artificially choose a dlog_base/ref_pt pair that'll result in the construction of a nonce point equal to the
+        # point-at-infinity if the proof's pub. hash & signature are both set arbitrarily to 1.
+        dlog_base1: ECC.EccPoint = self.context.G
+        ecc_pt1: ECC.EccPoint = -self.context.G * 1  # -G = -1 * G
+
+        base2_dlog: int = 7
+        dlog_base2: ECC.EccPoint = self.context.G * base2_dlog
+        ecc_pt2: ECC.EccPoint = dlog_base2 * self.common_dlog
+
+        dlog_params_set = NIZKDiscreteLogParameterSet(
+            self.context.curve_config,
+            [
+                ECCDiscreteLogBaseAndProduct((dlog_base1, ecc_pt1)),
+                ECCDiscreteLogBaseAndProduct((dlog_base2, ecc_pt2)),
+            ]
+        )
+
+        constant_proof_pub_hash = NIZKEqualDiscreteLogsProofHash(1)
+        constant_proof_sig = NIZKEqualDiscreteLogsProofSignature(1)
+
+        invalid_proof = NIZKEqualDiscreteLogsProof(
+            dlog_params_set,
+            constant_proof_pub_hash,
+            constant_proof_sig,
+            hash_algorithm=NIZKEqualDiscreteLogsContext.DEFAULT_HASH_ALGORITHM
+        )
+        print(f"NIZK Equal Discrete Logs Proof (Invalid Proof): {invalid_proof}\n")
+        self.assertFalse(invalid_proof.verify())
 
 
 if __name__ == '__main__':
