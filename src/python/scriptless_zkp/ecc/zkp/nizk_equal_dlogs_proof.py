c4301f5b4454936ba2a61f6f37b3faf7c927e580
Will D. Spann <qubits4all@users.noreply.github.com>
2024-04-21 02:21:16 -0600

    [zkp] Revise ECC point membership check
    - Revised the `nizk_equal_dlogs_proof` module's ECC point membership
    check in its `NIZKEqualDiscreteLogsContext` context class, to use the
    `WeierstrassEllipticCurveConfig` context class's new
    `is_point_on_curve(...)` method, instead of relying on an undocumented
    implementation detail of the PyCryptodome library, which is used for ECC
    elliptic curve operations.
    
    GI-15

--- src/python/scriptless_zkp/ecc/zkp/nizk_equal_dlogs_proof.py
+++ src/python/scriptless_zkp/ecc/zkp/nizk_equal_dlogs_proof.py
@@ -150,7 +150,10 @@ class NIZKEqualDiscreteLogsContext:
             return public_key.export_key(format='SEC1')
 
     def ecc_point_to_pubkey(self, ecc_point: ECC.EccPoint) -> ECC.EccKey:
-        return ECC.construct(curve=self.curve_config.curve, point_x=ecc_point.x, point_y=ecc_point.y)
+        if self.curve_config.is_point_on_curve(ecc_point):
+            return ECC.construct(curve=self.curve_config.curve, point_x=ecc_point.x, point_y=ecc_point.y)
+        else:
+            raise ValueError(f"Provided ECC point is not on the configured elliptic curve: '{self.curve_config.curve}'")
 
     def encode_ecc_point(self, ecc_point: ECC.EccPoint, point_compression: bool = False) -> bytes:
         """
