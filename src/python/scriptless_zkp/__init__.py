8a89c06e2ca5555b774bfdb18a0ea53b3de07e9f
Will D. Spann <qubits4all@users.noreply.github.com>
2024-06-09 02:13:19 -0600

    Add missing copyright notice
    - Added a missing copyright notice, referencing the repo's
    (copyleft) open-source MPL v2.0 license, to several source files.
    
    GI-16

--- src/python/scriptless_zkp/__init__.py
+++ src/python/scriptless_zkp/__init__.py
@@ -1,3 +1,17 @@
+###############################################################################
+# (c) 2024 W. Spann Systems Consulting
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
 """Common constants and type aliases used in the 'scriptless_zkp' package."""
 
 from typing import Literal
