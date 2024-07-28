594812ce28853b5872cd177e935471445bcc6422
Will D. Spann <qubits4all@users.noreply.github.com>
2024-07-28 00:09:38 -0600

    [test] Initial adaptor ECC Schnorr tests
    - Added initial unit-tests for the Adaptor ECC Schnorr signatures
    module, covering key-pair & tweak-pair generation, and pre-signature
    generation & verification.
    
    GI-23

---
+++ test/python/adaptor_ecc_schnorr_test.py
@@ -0,0 +1,78 @@
+import unittest
+
+from scriptless_zkp.ecc.signatures.adaptor_schnorr import AdaptorSchnorrContext, AdaptorSchnorrKeyPair, \
+    AdaptorSchnorrTweakPair, AdaptorSchnorrPreSignature, AdaptorSchnorrPublicKeys
+from scriptless_zkp.ecc.weierstrass_curves import WeierstrassEllipticCurveConfig
+
+
+class AdaptorECCSchnorrTests(unittest.TestCase):
+    context = AdaptorSchnorrContext(
+        WeierstrassEllipticCurveConfig.secp256r1(),
+        AdaptorSchnorrContext.DEFAULT_DOMAIN_SEPARATOR
+    )
+
+    def test_adaptor_Schnorr_key_generation(self):
+        key_pair = AdaptorSchnorrKeyPair.generate(self.context)
+
+        self.assertEqual(key_pair.context, self.context)
+        self.assertTrue(key_pair.ecc_key_pair.has_private())
+        self.assertFalse(key_pair.public_key.has_private())
+
+        # Verify `X != O` (i.e., the point-at-infinity).
+        self.assertFalse(key_pair.public_key_point.is_point_at_infinity())
+
+        # Verify `X = x * G`, where `x` is the private key scalar, `G` is the base point & `X` is the public key point.
+        self.assertEqual(self.context.curve_config.base_point * key_pair.private_key, key_pair.public_key_point)
+
+    def test_adaptor_tweak_pair_generation(self):
+        tweak_pair = AdaptorSchnorrTweakPair.generate(self.context)
+
+        self.assertEqual(tweak_pair.context, self.context)
+        self.assertTrue(tweak_pair.tweak_key_pair.has_private())
+        self.assertFalse(tweak_pair.public_tweak_key.has_private())
+
+        # Verify `Y != O` (i.e., the point-at-infinity).
+        self.assertFalse(tweak_pair.public_tweak.is_point_at_infinity())
+
+        # Verify `Y = y * G`, where `y` is the private tweak, `G` is the base point & `Y` is the public tweak point.
+        self.assertEqual(self.context.curve_config.base_point * tweak_pair.private_tweak, tweak_pair.public_tweak)
+
+    def test_adaptor_signature_generation(self):
+        key_pair = AdaptorSchnorrKeyPair.generate(self.context)
+        tweak_pair = AdaptorSchnorrTweakPair.generate(self.context)
+
+        test_message: bytes = b'foo'
+        signature: AdaptorSchnorrPreSignature = key_pair.sign(tweak_pair.public_tweak_key, test_message)
+
+        self.assertEqual(signature.context, self.context)
+
+        # Verify `R' != O` (i.e., the point-at-infinity).
+        self.assertFalse(signature.public_nonce.is_point_at_infinity())
+
+        # Verify pre-signature scalar value is non-zero.
+        self.assertNotEqual(signature.pre_signature, 0, "Pre-signature scalar value should not be zero.")
+
+        # Verify `s' * G != O` (i.e., the point-at-infinity).
+        self.assertFalse((self.context.curve_config.base_point * signature.pre_signature).is_point_at_infinity())
+
+    def test_adaptor_signature_verification(self):
+        key_pair = AdaptorSchnorrKeyPair.generate(self.context)
+        tweak_pair = AdaptorSchnorrTweakPair.generate(self.context)
+
+        adaptor_pub_keys = AdaptorSchnorrPublicKeys(self.context, key_pair.public_key, tweak_pair.public_tweak_key)
+
+        test_message: bytes = b'foo'
+        pre_sig: AdaptorSchnorrPreSignature = key_pair.sign(tweak_pair.public_tweak_key, test_message)
+
+        # Verify the pre-signature via the verify_pre_signature(...) method on the public keys object.
+        self.assertTrue(
+            adaptor_pub_keys.verify_pre_signature(pre_sig, test_message),
+            "Pre-signature verification failed."
+        )
+
+        # Verify the pre-signature via the verify(...) helper method on the pre-signature object.
+        self.assertTrue(pre_sig.verify(adaptor_pub_keys, test_message))
+
+
+if __name__ == '__main__':
+    unittest.main()
