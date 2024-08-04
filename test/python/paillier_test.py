582dfa3f7d959b4871d1fc88776bc57d53d7467d
Will D. Spann <qubits4all@users.noreply.github.com>
2024-08-04 17:43:09 -0600

    [test] Print public/private key-pairs in Paillier tests
    - Added `print(...)` statements to Paillier unit tests to print the
    public/private key-pairs' base64-encoding via the associated
    `encode_to_base64()` methods.
    
    GI-20

--- test/python/paillier_test.py
+++ test/python/paillier_test.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
+
 import secrets
 import unittest
 
@@ -18,14 +19,21 @@ from scriptless_zkp.he.paillier import PaillierKeyPair, MIN_KEY_SIZE
 
 
 class PaillierHomomorphicEncryptionTests(unittest.TestCase):
-    key_size: int = MIN_KEY_SIZE
-    key_pair = PaillierKeyPair.generate(key_size_bits=key_size)
+    test_key_size: int = MIN_KEY_SIZE
+    test_key_pair: PaillierKeyPair = PaillierKeyPair.generate(key_size_bits=test_key_size)
+
     small_test_msg: int = 42
-    large_test_msg: int = secrets.randbelow(key_pair.public_key.n)
-    assert large_test_msg > 0
+    while (large_test_msg := secrets.randbelow(test_key_pair.public_key.n)) == 0:
+        pass
 
     def test_key_pair_generation(self):
-        key_pair = PaillierKeyPair.generate(key_size_bits=self.key_size)
+        key_pair = PaillierKeyPair.generate(key_size_bits=self.test_key_size)
+
+        # Print the base64-encoded public and private keys.
+        print(
+            f"\nPublic Key (n, g): {key_pair.public_key.encode_to_base64()}\n"
+            f"Private Key (λ, n): {key_pair.private_key.encode_to_base64()}"
+        )
 
         # Check if: g^λ == 1 mod n
         self.assertEqual(
@@ -43,34 +51,58 @@ class PaillierHomomorphicEncryptionTests(unittest.TestCase):
         )
 
     def test_encryption_validity_small_message(self):
-        ciphertext = self.key_pair.public_key.encrypt(self.small_test_msg)
+        # Print the base64-encoded public and private keys.
+        print(
+            f"\nPublic Key (n, g): {self.test_key_pair.public_key.encode_to_base64()}\n"
+            f"Private Key (λ, n): {self.test_key_pair.private_key.encode_to_base64()}"
+        )
+
+        ciphertext = self.test_key_pair.public_key.encrypt(self.small_test_msg)
 
         # Check if the ciphertext is a valid encryption of the plaintext.
         self.assertGreater(ciphertext.encrypted, 0, "The encrypted value should be a positive integer.")
         self.assertLess(
-            ciphertext.encrypted, self.key_pair.public_key.n2, "The encrypted value should be less than n^2."
+            ciphertext.encrypted, self.test_key_pair.public_key.n2, "The encrypted value should be less than n^2."
         )
 
     def test_encryption_validity_large_message(self):
-        ciphertext = self.key_pair.public_key.encrypt(self.large_test_msg)
+        # Print the base64-encoded public and private keys.
+        print(
+            f"\nPublic Key (n, g): {self.test_key_pair.public_key.encode_to_base64()}\n"
+            f"Private Key (λ, n): {self.test_key_pair.private_key.encode_to_base64()}"
+        )
+
+        ciphertext = self.test_key_pair.public_key.encrypt(self.large_test_msg)
 
         # Check if the ciphertext is a valid encryption of the plaintext.
         self.assertGreater(ciphertext.encrypted, 0, "The encrypted value should be a positive integer.")
         self.assertLess(
-            ciphertext.encrypted, self.key_pair.public_key.n2, "The encrypted value should be less than n^2."
+            ciphertext.encrypted, self.test_key_pair.public_key.n2, "The encrypted value should be less than n^2."
         )
 
     def test_encryption_decryption_small_message(self):
-        ciphertext = self.key_pair.public_key.encrypt(self.small_test_msg)
+        # Print the base64-encoded public and private keys.
+        print(
+            f"\nPublic Key (n, g): {self.test_key_pair.public_key.encode_to_base64()}\n"
+            f"Private Key (λ, n): {self.test_key_pair.private_key.encode_to_base64()}"
+        )
 
-        decrypted = self.key_pair.private_key.decrypt(ciphertext)
+        ciphertext = self.test_key_pair.public_key.encrypt(self.small_test_msg)
+
+        decrypted = self.test_key_pair.private_key.decrypt(ciphertext)
 
         self.assertEqual(self.small_test_msg, decrypted, "The decrypted value should match the original plaintext.")
 
     def test_encryption_decryption_large_message(self):
-        ciphertext = self.key_pair.public_key.encrypt(self.large_test_msg)
+        # Print the base64-encoded public and private keys.
+        print(
+            f"\nPublic Key (n, g): {self.test_key_pair.public_key.encode_to_base64()}\n"
+            f"Private Key (λ, n): {self.test_key_pair.private_key.encode_to_base64()}"
+        )
 
-        decrypted = self.key_pair.private_key.decrypt(ciphertext)
+        ciphertext = self.test_key_pair.public_key.encrypt(self.large_test_msg)
+
+        decrypted = self.test_key_pair.private_key.decrypt(ciphertext)
 
         self.assertEqual(self.large_test_msg, decrypted, "The decrypted value should match the original plaintext.")
 
